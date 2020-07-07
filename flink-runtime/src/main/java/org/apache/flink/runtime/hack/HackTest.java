40054610c60fca598ab6d1350bbfd78907465712
xia rui <xiarui@smail.nju.edu.cn>
2020-07-07 19:59:09 +0800

    First hack attempt

--- flink-runtime/src/main/java/org/apache/flink/runtime/hack/HackTest.java
+++ flink-runtime/src/main/java/org/apache/flink/runtime/hack/HackTest.java
@@ -1,5 +1,8 @@
 package org.apache.flink.runtime.hack;
 
+/**
+ * This is a HackTest for Flink Hacking
+ */
 public class HackTest {
 	public static void hackTest() {
 		System.out.println("This is my first hack");
